ad7284a7ed9b2b636af88a69fe14a6410a418ba4
dinever <dingpeixuan911@gmail.com>
2016-05-23 01:49:57 -0400

    Use validate.js to pop up error notification.

--- view/admin/assets/src/js/signup.js
+++ view/admin/assets/src/js/signup.js
@@ -1,20 +1,21 @@
 $(function () {
-  new FormValidator("signup-form", [
-      {"name": "password", "rules": "required|min_length[4]|max_length[20]"}
-  ], function (errors, e) {
-    e.preventDefault();
-    $('.invalid').hide();
-    if (errors.length) {
-      $("#" + errors[0].id + "-invalid").removeClass("hide").show();
-      return;
-    }
-    $('#signup-form').ajaxSubmit({
-      success: function (json) {
-        window.location.href = "/admin/";
-      },
-      error: function (json) {
-        alertify.error(("Error: " + JSON.parse(json.responseText).msg));
-      }
-    });
-  })
+    new FormValidator("signup-form", [
+        {"name": "name", "rules": "required"},
+        {"name": "email", "rules": "required"},
+        {"name": "password", "rules": "required|min_length[4]|max_length[20]"}
+    ], function (errors, e) {
+        e.preventDefault();
+        if (errors.length) {
+            alertify.error("Error: " + errors[0].message);
+            return;
+        }
+        $('#signup-form').ajaxSubmit({
+            success: function (json) {
+                window.location.href = "/admin/";
+            },
+            error: function (json) {
+                alertify.error(("Error: " + JSON.parse(json.responseText).msg));
+            }
+        });
+    })
 });
