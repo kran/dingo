4692b9fb59386054cbe520aa7d1ba9f2998722ca
Ben Tranter <bwtrante@lakeheadu.ca>
2016-05-25 19:29:49 -0400

    Document util package

--- app/utils/avatar.go
+++ app/utils/avatar.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 )
 
-// generate gravatar link by email.
+// Gravatar generates a Gravatar link with a given email and size. Images
+// are always square, so we only accept a single argument for the size.
 func Gravatar(email string, size string) string {
 	u := "http://1.gravatar.com/avatar/"
 	u += encodeAvatarEmail(email) + "?s=" + size
